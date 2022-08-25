6c2bad7d63b26e4dd36b0c58f4ef5ba9af825ed2
Colton Randall <colton.randall@xero.com>
2022-08-25 21:54:30 +1200

    added bootstrap and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 import "./sass/App.scss";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Navbar />
       <h3>Colton Randall's full-stack to do list WOHOO!</h3>
     </div>
   );
--- src/components/Home.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-const Home = () => {
-  return <h1>This is a Navbar</h1>;
-};
-
-export default Home;
