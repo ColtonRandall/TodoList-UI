d224821cc01545bd5329ba2cb87615c21d211537
Colton Randall <colton.randall@xero.com>
2022-08-25 21:13:23 +1200

    restructured project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders colton's title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const text = "Colton Randall's full-stack to do list";
+  const linkElement = screen.getByText(text);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/Home";
+import "./sass/App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
+      <h3>Colton Randall's full-stack to do list</h3>
     </div>
   );
 }
